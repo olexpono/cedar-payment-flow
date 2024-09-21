b15f35fb7248c2f94481c558c022deded701332e
Olex Ponomarenko <olex.pono@gmail.com>
2024-09-21 07:40:03 -0500

    --amend

---
+++ src/pages/StepThanks.tsx
@@ -0,0 +1,11 @@
+import Container from "../components/Container";
+
+function StepThanks() {
+  return (
+    <Container variant="transparent">
+      <h2>Thank you for your payment!</h2>
+    </Container>
+  );
+}
+
+export default StepThanks;
